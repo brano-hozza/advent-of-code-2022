ec01468c20a68f8e9c3c67d5aefa2a18e87f8457
Branislav Hozza <brankohozza@gmail.com>
2022-12-04 21:00:01 +0100

    :sparkles: prepare empty projects for next days

---
+++ rust-version/day-03-b/src/main.rs
@@ -0,0 +1,25 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn main() {
+    // File hosts must exist in current path before this produces output
+    if let Ok(lines) = read_lines("./input.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        let mut points: u32 = 0;
+        for line in lines {
+            if let Ok(value) = line {}
+        }
+        println!("{}", points);
+    }
+}
