2be45c6e975b2fe5cca4df67f92b38faef824602
Branislav Hozza <brankohozza@gmail.com>
2022-12-04 21:25:45 +0100

    :sparkles: rust impl day 3 part 1

--- rust-version/day-03-a/src/main.rs
+++ rust-version/day-03-a/src/main.rs
@@ -16,10 +16,28 @@ fn main() {
     // File hosts must exist in current path before this produces output
     if let Ok(lines) = read_lines("./input.txt") {
         // Consumes the iterator, returns an (Optional) String
-        let mut points: u32 = 0;
+        let mut total: u32 = 0;
         for line in lines {
-            if let Ok(value) = line {}
+            if let Ok(value) = line {
+                let length = value.len();
+                let first_half = &value[0..(length / 2)];
+                let second_half = &value[(length / 2)..length];
+                let unique_first: Vec<char> = first_half.chars().collect();
+                let mut char = unique_first[0];
+                for c in second_half.chars() {
+                    if unique_first.contains(&c) {
+                        char = c;
+                        break;
+                    }
+                }
+                let ascii_value = char as u32;
+                if ascii_value > 96 {
+                    total += ascii_value - 96;
+                } else {
+                    total += ascii_value - 38;
+                }
+            }
         }
-        println!("{}", points);
+        println!("{}", total);
     }
 }
