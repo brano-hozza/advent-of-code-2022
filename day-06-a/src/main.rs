05fc55c3147d6ab1ae3e795204ae0a986e66498f
Branislav Hozza <brankohozza@gmail.com>
2022-12-07 23:48:51 +0100

    :sparkles: day 6 part A :christmas_tree:

--- day-06-a/src/main.rs
+++ day-06-a/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -15,7 +16,23 @@ fn main() {
     // File hosts must exist in current path before this produces output
     if let Ok(lines) = read_lines("./input.txt") {
         for line in lines {
-            if let Ok(value) = line {}
+            if let Ok(value) = line {
+                let chars = value.chars();
+                let mut stack_4: Vec<char> = Vec::new();
+                let mut index = 0;
+                for char in chars {
+                    index += 1;
+                    stack_4.push(char);
+                    if stack_4.len() == 4 {
+                        let uniques: HashSet<char> = HashSet::from_iter(stack_4.iter().cloned());
+                        if uniques.len() == 4 {
+                            println!("{}", index);
+                            return;
+                        }
+                        stack_4.remove(0);
+                    }
+                }
+            }
         }
     }
 }
