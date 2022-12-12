0a75997ae81e3c0200d0139a98180dfa17a5d092
Branislav Hozza <brankohozza@gmail.com>
2022-12-12 15:56:20 +0100

    :sparkles: prepare dirs for 9,10,11,12

---
+++ day-10-a/src/main.rs
@@ -0,0 +1,22 @@
+use std::borrow::BorrowMut;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn main() {
+    if let Ok(lines) = read_lines("./input.txt") {
+        for line in lines {
+            if let Ok(value) = line {}
+        }
+    }
+}
